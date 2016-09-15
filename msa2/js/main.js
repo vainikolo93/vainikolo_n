bfd3d3ec6f5f3ce639c48b090211e78bff45ccb7
Nancy Vainikolo <nancyvainikolo@gmail.com>
2016-09-15 22:01:42 +1200

    test-login

--- msa2/js/main.js
+++ msa2/js/main.js
@@ -1,3 +1,11 @@
+ (function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.7&appId=1680685105584496";
+                    fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'facebook-jssdk'));
+
   // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
     console.log('statusChangeCallback');
