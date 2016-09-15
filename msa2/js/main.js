27801e90e100867a54314b430ab7bc56b93c5e5d
Nancy Vainikolo <nancyvainikolo@gmail.com>
2016-09-15 19:28:27 +1200

    Test Login Form

--- msa2/js/main.js
+++ msa2/js/main.js
@@ -2,5 +2,19 @@ function clicked(){
 
     let user = document.getElementById('username');
     let pass = document.getElementById('password');
-    
+
+    let coruser = "test";
+    let corpass = "123";
+
+    if(user.value == coruser){
+        if(pass.value == corpass){
+            window.alert("You are logged in as" + user.value);
+        }else {
+            window.alert("Incorrect username or password!");
+        }
+    }else{
+         window.alert("Incorrect username or password!");
+    }
+
+
 }
